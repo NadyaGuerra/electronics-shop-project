91c4058ce70620c83f91fc2d808b8d5f92c302f0
Nadya Guerra <nadyammex@gmail.com>
2023-05-10 10:23:23 -0700

    home_5 error

--- homework-5/main.py
+++ homework-5/main.py
@@ -6,11 +6,11 @@ if __name__ == '__main__':
 
     assert str(kb.language) == "EN"
 
-    kb.change_lang
+    kb.change_lang()
     assert str(kb.language) == "RU"
 
     # Сделали RU -> EN -> RU
-    kb.change_lang.change_lang
+    kb.change_lang().change_lang()
     assert str(kb.language) == "RU"
 
     kb.language = 'CH'
